fcecab3089e7e3d4f8d278ec7e6ba0dc2ee42c65
Carlos Nascimento <pelezinho@linux-gpt3.suse>
2018-08-13 14:08:54 -0300

    Frontend

--- src/main/java/br/com/inmetrics/desafiokernel/controllers/BucketController.java
+++ src/main/java/br/com/inmetrics/desafiokernel/controllers/BucketController.java
@@ -1,23 +1,29 @@
 package br.com.inmetrics.desafiokernel.controllers;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import br.com.inmetrics.desafiokernel.services.BucketService;
 
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
-@RequestMapping("/api/bucket")
+@RequestMapping("/api")
 public class BucketController {
 	
 	@Autowired
 	private BucketService service;
 	
-	@RequestMapping(value = "/list", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
-	public Iterable<S3ObjectSummary> list() throws IOException {
+//	@RequestMapping(value = "/list", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
+	@GetMapping
+	public List<S3ObjectSummary> list() throws IOException {
 		return service.list();
 	}	
 }
